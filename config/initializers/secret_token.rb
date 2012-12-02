a79e1efbe2caeba22f50d2aec4fdbe1ee258b369
nazarvynn <nazar.vynn@gmail.com>
2012-12-02 16:37:31 +0200

    Add RoR files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd41d65284dad32a62481a4143de65929470c3c0040f79d255995cb2823b791432a13f60d51edaaed91648893aab0e7a6d41965c3832884790bbd404761358a03'
