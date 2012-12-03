acbdf1358a03acbb5d98743b7337e5cafa776274
nazarvynn <nazar.vynn@gmail.com>
2012-12-03 19:58:01 +0200

    Added new test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,10 +7,17 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-    it "should have the title 'Home'" do
+
+    it "should have base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                  :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :title => 'Ruby on Rails Tutorial Sample App')
+    end
+
+    it "should not have custom title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                                :text => "| Home")
     end
   end
 
