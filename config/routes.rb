1a334c412a96f070b104add81c8d5fd9e32a95ed
nazarvynn <nazar.vynn@gmail.com>
2012-12-04 18:41:19 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
 
-  get "user/new"
-
   root to: 'static_pages#home'
+
+  match '/signup',  to: 'user#new'
 
   match '/about',   to: 'static_pages#about'
   match '/help',    to: 'static_pages#help'
