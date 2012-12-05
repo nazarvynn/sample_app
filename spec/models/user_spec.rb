2b0c773548360ee64f4a92badd3b5dd18fe9252f
nazarvynn <nazar.vynn@gmail.com>
2012-12-05 16:04:13 +0200

    Add email rspec-s

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,13 +14,17 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(:name => "Example User", :email => "user@example.com")
+    @user = User.new(:name => "Example User", :email => "user@example.com",
+                     :password => "foobar", :password_confirmation => "foobar")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -66,6 +70,21 @@ describe User do
       user_with_sane_email.save
     end
 
+    it { should_not be_valid }
+  end
+
+  discribe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password is doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 end
