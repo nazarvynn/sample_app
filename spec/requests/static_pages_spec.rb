2cd8f3899925531e41d5041c08dbbb396ba6fa23
nazarvynn <nazar.vynn@gmail.com>
2012-12-06 17:29:44 +0200

    Implemented edit functionality

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,8 +8,8 @@ describe "StaticPages" do
     before { visit root_path }
 
     it { should have_selector('h1',   :text => 'Sample App') }
-    it { should have_selector('title',:text => full_title('')) }
-    it { should_not have_selector('title',  :text => full_title('| Home')) }
+    it { should have_selector('title', :text => full_title('')) }
+    it { should_not have_selector('title', :text => full_title('Home')) }
   end
 
 
@@ -34,15 +34,5 @@ describe "StaticPages" do
     it { should have_selector('h1',     :text => 'Contact') }
     it { should have_selector('title',  :text => full_title('Contact')) }
   end
-
-=begin
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
-=end
 
 end
